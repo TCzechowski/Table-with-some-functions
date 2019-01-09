b62ed6840cb04751613674756cfeaaafda827031
TCzechowski <tczechowski5@gmail.com>
2019-01-09 22:37:51 +0100

    Updated index.js
    Modified sorting function to sort dateOfBirth property by timestamps, added function that modifies dateOfBirth properties to have same format.

--- index.js
+++ index.js
@@ -18,9 +18,18 @@ let sicedPeople = [];
 fetch(endpoint)
   .then(blob => blob.json())
   .then(data => people.push(...data))
+  .then(() => people.map(elem =>
+    elem.dateOfBirth = leadingZero(elem.dateOfBirth)
+  ))
   .then(() => sliceArray(people, 5))
   .then(() => displayContent(0))
   .then(() => sortListeners());
+
+
+function leadingZero(element) {
+  const splitElement = element.split('.');
+  return splitElement.map(e => e.length === 1 ? '0' + e : e).join('.');
+}
 
 function displayContent(page) {
   let suggestions = document.querySelector('.suggestions');
@@ -35,13 +44,13 @@ function displayContent(page) {
     <td>${person.experience}</td>
     </tr>
     `}).join("");
-    suggestions.innerHTML = html;
-    createButtons();
-  };
+  suggestions.innerHTML = html;
+  createButtons();
+};
 
 function sliceArray(arr, size) {
   slicedPeople = [];
-  for (i=0; i < arr.length; i += size){
+  for (i = 0; i < arr.length; i += size) {
     slicedPeople.push(arr.slice(i, i + size));
   }
   return slicedPeople;
@@ -50,11 +59,11 @@ function sliceArray(arr, size) {
 function createButtons() {
   const container = document.querySelector("#table1");
 
-  for (i=0; i<slicedPeople.length; i++){
+  for (i = 0; i < slicedPeople.length; i++) {
     const btn = document.createElement('button');
 
     btn.classList.add('page-button');
-    btn.innerText = `${i+1}`;
+    btn.innerText = `${i + 1}`;
     btn.setAttribute('data-page', i);
 
     container.appendChild(btn);
@@ -62,31 +71,48 @@ function createButtons() {
   switchPage();
 }
 
-function switchPage(){
+function switchPage() {
   const buttons = document.querySelectorAll('.page-button');
-  for(i=0; i < buttons.length; i++){
+  for (i = 0; i < buttons.length; i++) {
     let button = buttons[i];
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
       const pageNumber = this.dataset.page;
       displayContent(pageNumber);
-      ;});
+      ;
+    });
   }
 }
 
-function sortListeners(){
+function sortListeners() {
   const elements = document.querySelectorAll('.table-head');
 
-  for(i=0; i < elements.length; i++){
+  for (i = 0; i < elements.length; i++) {
     let element = elements[i];
     element.addEventListener('click', sortArray);
   }
 }
 
+function parseDate(element) {
+  const splitElement = element.split(' ');
+  return splitElement[0].split('.').reverse().join('.') + ' ' + splitElement[1];
+}
+
 let result = 1;
-function sortArray(){
+function sortArray() {
   const elementProperty = this.dataset.property;
+
   if (elementProperty === 'dateOfBirth') {
-    console.log('DOB');
+    people.sort((a, b) => {
+
+      const x = new Date(parseDate(a.dateOfBirth)).getTime();
+      const y = new Date(parseDate(b.dateOfBirth)).getTime();
+
+      return x > y ? [result] : -[result];
+    });
+    sliceArray(people, 5);
+    displayContent(0);
+    result = -result;
+
   } else {
     people.sort((a, b) => a[elementProperty] > b[elementProperty] ? [result] : -[result]);
     sliceArray(people, 5);
@@ -94,3 +120,4 @@ function sortArray(){
     result = -result;
   }
 }
+
