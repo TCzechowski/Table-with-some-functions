0f85c9873864108bd4a93d69481d40322971a68e
TCzechowski <tczechowski5@gmail.com>
2019-01-18 23:23:11 +0100

    Added styling and refactored some funtcions.
    Added styling and refactored some funtcions for better reusability.

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-// jQuery Datepicker
-// $(".datepicker").datepicker({
-//   defaultDate: null,
-//   prevText: "<",
-//   nextText: ">",
-//   changeMonth: true,
-//   changeYear: true,
-//   firstDay: 1,
-//   yearRange: "1930:2017",
-//   dateFormat: "dd.mm.yy",
-//      onSelect: function(dateText, inst) {
-//             $(".search").val(dateText);
-//         }
-// });
 const endpoint = 'https://api.myjson.com/bins/1f4b4c';
 let people = [];
-let sicedPeople = [];
-let filteredPeople;
+let slicedPeople = [];
+let filteredPeople = [];
 fetch(endpoint)
-.then(blob => blob.json())
-.then(data => people.push(...data))
-.then(() => people.map(elem =>
-  elem.dateOfBirth = leadingZero(elem.dateOfBirth)
+  .then(blob => blob.json())
+  .then(data => people.push(...data))
+  .then(() => people.map(elem =>
+    elem.dateOfBirth = leadingZero(elem.dateOfBirth)
   ))
   .then(() => init());
 
-  function init() {
-    sliceArray(people, 5);
-    displayContent(0);
-    displayMatches();
-  }
+function init() {
+  sliceArray(people, 5);
+  displayContent(0);
+  displayMatches();
+  createButtons();
+  pageActive(0);
+}
 
+function leadingZero(element) {
+  const splitElement = element.split('.');
+  return splitElement.map(e => e.length === 1 ? '0' + e : e).join('.');
+}
 
-  function leadingZero(element) {
-    const splitElement = element.split('.');
-    return splitElement.map(e => e.length === 1 ? '0' + e : e).join('.');
-  }
-  function parseDate(element) {
-    const splitElement = element.split(' ');
-    return splitElement[0].split('.').reverse().join('.') + ' ' + splitElement[1];
-  }
+function parseDate(element) {
+  const splitElement = element.split(' ');
+  return splitElement[0].split('.').reverse().join('.') + ' ' + splitElement[1];
+}
 
-  function displayContent(page) {
-  let suggestions = document.querySelector('.suggestions');
+function displayContent(page) {
+  let suggestions = document.querySelector('.table__content');
   let html = slicedPeople[page].map(person => {
     return `
-    <tr>
+    <tr class="table__item">
     <td>${person.id}</td>
     <td>${person.firstName}</td>
     <td>${person.lastName}</td>
@@ -54,25 +42,29 @@ fetch(endpoint)
     </tr>
     `}).join("");
   suggestions.innerHTML = html;
-  createButtons();
 };
 
-const sliceArray = function (arr, size) {
+function sliceArray(arr, size) {
   slicedPeople = [];
-  for (i = 0; i < arr.length; i += size) {
-    console.log(i);
+  for (let i = 0; i < arr.length; i += size) {
     slicedPeople.push(arr.slice(i, i + size));
   }
   return slicedPeople;
 }
 
 function createButtons() {
-  const container = document.querySelector("#table1");
+  const container = document.querySelector('.page-buttons');
+  const btnChildren = document.querySelectorAll('.page-buttons__button');
 
+  //remove existing buttons
+  for (i = btnChildren.length; i > 0; i--) {
+    document.querySelectorAll('.page-buttons__button')[0].remove();
+  }
+  //create new buttons
   for (i = 0; i < slicedPeople.length; i++) {
     const btn = document.createElement('button');
 
-    btn.classList.add('page-button');
+    btn.classList.add('page-buttons__button');
     btn.innerText = `${i + 1}`;
     btn.setAttribute('data-page', i);
 
@@ -81,14 +73,22 @@ function createButtons() {
   switchPageListeners();
 }
 
+function pageActive(pageNumber) {
+  const buttons = Array.from(document.querySelectorAll('[data-page]'));
+  buttons.forEach(e => e.classList.remove('page-buttons__button--active'));
+
+  const activeBtn = buttons[`${pageNumber}`];
+  activeBtn.classList.add('page-buttons__button--active');
+}
+
 function switchPageListeners() {
-  const buttons = document.querySelectorAll('.page-button');
-  for (i = 0; i < buttons.length; i++) {
+  const buttons = document.querySelectorAll('.page-buttons__button');
+  for (let i = 0; i < buttons.length; i++) {
     let button = buttons[i];
     button.addEventListener('click', function () {
       const pageNumber = this.dataset.page;
       displayContent(pageNumber);
-      ;
+      pageActive(pageNumber);
     });
   }
 }
@@ -129,44 +129,44 @@ function findMatches(wordToMatch, arr) {
   });
 }
 
-function displayMatches(){
+function displayMatches() {
   findMatches(this.value, people);
   sliceArray(filteredPeople, howManyResults());
   displayContent(0);
+  createButtons();
+  pageActive(0);
 }
 
-function howManyResults(){
-  const resultsInput = document.querySelector('.results-per-page');
-  const inputLength = resultsInput.value.length;
-  const results = parseInt(resultsInput.value);
-  if (inputLength == 0 || results === '0') {
-    return 5;
-  } else {
-    return results;
-  }
+function howManyResults() {
+  const resultsInput = document.querySelector('.table__results');
+  return results = parseInt(resultsInput.value);
 }
 
-function resultsPerPage(){
+function resultsPerPage() {
   sliceArray(filteredPeople, howManyResults());
   displayContent(0)
+  createButtons();
+  pageActive(0);
 }
 
 function sortListeners() {
-  const elements = document.querySelectorAll('.table-head');
+  const elements = document.querySelectorAll('.table__header');
 
-  for (i = 0; i < elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     let element = elements[i];
     element.addEventListener('click', sortArray);
   }
 }
 
+
+
 // EVENT LISTENERS
 document.addEventListener('DOMContentLoaded', sortListeners);
 
-const searchInput = document.querySelector('.search');
+const searchInput = document.querySelector('.table__search');
 searchInput.addEventListener('keyup', displayMatches);
 searchInput.addEventListener('change', displayMatches);
 
-const perPageInput = document.querySelector('.results-per-page');
+const perPageInput = document.querySelector('.table__results');
 perPageInput.addEventListener('keyup', resultsPerPage);
 perPageInput.addEventListener('change', resultsPerPage);
